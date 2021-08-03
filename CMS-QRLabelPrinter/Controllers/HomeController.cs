0f3cb2c588c47f9951e2e7f6effe3ddf09da5bf3
Bilal <bilalahmed90@yahoo.com>
2021-08-03 20:45:46 +0100

    Connected to CMS_db and added a POST action controller with insert SQL query with dummy data for testing purposes, test successful!
    Also removed  custom 'oninvalid' from form input, it was causing a bug to loop into itself even after entering valid 11 digit code and the only way to get rid of it was to refresh the page.

--- CMS-QRLabelPrinter/Controllers/HomeController.cs
+++ CMS-QRLabelPrinter/Controllers/HomeController.cs
@@ -5,13 +5,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMS_QRLabelPrinter.Models;
+using System.Data.SqlClient;
 
 namespace CMS_QRLabelPrinter.Controllers
 {
     public class HomeController : Controller
     {
+
+        // Bitmap bit = null; // Global variable to store QR code as a bitmap image
+        bool customerInfo = false; // Variable to determine whether to print additional customer info
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string qrText)
+        {
+
+            string constr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CMS_db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                string query = "INSERT INTO [QRTable] (JobKeyID, ItemKeyID, ScanDate) VALUES (1, 1, GETDATE())";
+
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = conn;
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        return Content("SQL Error: " + e.Message.ToString());
+                    }
+
+                }
+            }
             return View();
         }
 
